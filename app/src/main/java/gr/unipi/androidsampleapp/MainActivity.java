8e21f2a8430e4bbaeb47cc4a60acf76a59cdbb18
Andreas Menychtas <amenychtas@gmail.com>
2017-03-22 03:44:43 +0200

    preparing lecture 3

--- app/src/main/java/gr/unipi/androidsampleapp/MainActivity.java
+++ app/src/main/java/gr/unipi/androidsampleapp/MainActivity.java
@@ -1,14 +1,24 @@
 package gr.unipi.androidsampleapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
     private TextView helloTextView;
     private Button blueButton;
+    private Locale locale = Locale.getDefault();
     //Keep in mind that there is no need to add a reference to a red button!
 
     @Override
@@ -29,10 +39,56 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.menu_main_language:
+                changeLocale();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
     /**
      * Makes the text color red.
      */
     public void changeColorToRed(View view) {
         helloTextView.setTextColor(getResources().getColor(R.color.colorUnipiRed));
+    }
+
+    /**
+     * Changes the locate of the application
+     */
+    private void changeLocale() {
+        if (locale.getLanguage().equals("el")) {
+            locale = new Locale("en");
+        } else if (locale.getLanguage().equals("en")) {
+            locale = new Locale("el");
+        }
+
+        Locale.setDefault(locale);
+
+        Resources resources = getResources();
+        DisplayMetrics displayMetrics = resources.getDisplayMetrics();
+        Configuration configuration = resources.getConfiguration();
+        configuration.setLocale(locale);
+        resources.updateConfiguration(configuration, displayMetrics);
+
+        //We can check the version api with
+        //if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
+        //}
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
     }
 }
